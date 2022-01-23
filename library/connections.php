6a18462269b3853ace4bd3b8b32df70b7bc3ecdd
Alirio-Mieres <mie21001@byui.edu>
2022-01-22 22:18:49 -0400

    Activities week 3

---
+++ library/connections.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+* Proxy connection to the phpmotors database
+*/
+function phpmotorsConnect(){ 
+
+$server = "mysql";
+$dbname = 'phpmotors';
+$username = 'iClient';
+$password = 'E-GNo2c*ocp0kFlJ';
+$dsn = "mysql:host=$server;dbname=$dbname";
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+try{
+  $link = new PDO($dsn, $username, $password, $options);
+  /*if (is_object($link)) {
+    echo "It worked!";
+  }*/
+  return $link;
+
+ } catch (PDOException $e) {
+    header('Location: /phpmotors/view/500.php');
+    exit;
+  }
+}
+phpmotorsConnect();
+
+
+?>
+
